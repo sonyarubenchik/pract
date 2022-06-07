6a56feb8149404a399021917a82e83f7cb2326f9
Sonya <Sonya100sonya@gmail.com>
2022-06-07 11:18:56 +0300

    test

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -3,7 +3,8 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
- # post views
- path('', views.post_list, name='post_list'),
- path('<int:id>/', views.post_detail, name='post_detail'),
+    # post views
+    path('', views.post_list, name='post_list'),
+    path('<int:id>/', views.post_detail, name='post_detail'),
+    path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
 ]
